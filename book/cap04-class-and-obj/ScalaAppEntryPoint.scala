229d36338fc008410b61d3800de7263024b21c48
Allan Silva <allan@allansilva.com.br>
2022-11-02 11:57:58 -0300

    An scala with main method entrypoit app

--- book/cap04-class-and-obj/ScalaAppEntryPoint.scala
+++ book/cap04-class-and-obj/ScalaAppEntryPoint.scala
@@ -1,2 +1,10 @@
 import ChecksumAccumulator.calculate
 
+// To this work:
+  // 1) compile deps scalac ChecksumAccumulator.scala
+  // 2) Run scala ScalaAppEntryPoint.scala Hey, teacher, leave them kids alone
+
+object Summer:
+  def main(args: Array[String]): Unit =
+    for arg <- args do
+      println(arg + ": " + calculate(arg))
